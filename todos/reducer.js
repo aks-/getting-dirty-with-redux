d5353a9034431c8200304a8a004749e09c57ff2f
aks- <coderatlabs@gmail.com>
2015-12-09 12:57:23 +0530

    implement combineReducer

--- todos/reducer.js
+++ todos/reducer.js
@@ -43,7 +43,23 @@ const visibilityFilter = (
   }
 };
 
-const {createStore, combineReducers} = Redux;
+const {createStore} = Redux;
+
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {}
+    );
+  };
+};
+
 const todoApp = combineReducers({
   todos,
   visibilityFilter
